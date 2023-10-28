ebec073e090c1b1e842810ea5c58a28940b21038
Rudra <prajapatirudra127@gmail.com>
2023-10-28 18:42:23 +0530

    404 not found

--- src/404Notfound/404page.js
+++ src/404Notfound/404page.js
@@ -7,7 +7,7 @@ const NotFound = () => {
       <h1 className="text-4xl text-blue-500">404 - Page Not Found</h1>
       <p className="text-gray-600">The page you&apos;re looking for does not exist.</p>
       
-      {/* Replace the NavLink with a button for "Back to Home" */}
+     
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded">
         <NavLink to="/">Back to Home</NavLink>
       </button>
