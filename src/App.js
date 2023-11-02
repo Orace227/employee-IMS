36e7e5a0acec6750b32cc8c4463da306a2a16725
Rudra <prajapatirudra127@gmail.com>
2023-11-02 23:52:19 +0530

    added funcwhere if cookie there dontask idpassword

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { CssBaseline, StyledEngineProvider } from '@mui/material';
 // import index from 'index.css';
 import { Route } from 'react-router-dom';
 import {useNavigate} from 'react-router-dom';
-// routing
+// routing  
 import Routes from 'routes';
 
 // defaultTheme
 import themes from 'themes';
 
 // project imports
-import NavigationScroll from 'layout/NavigationScroll';
+import NavigationScroll from './layout/NavigationScroll';
 
 import 'tailwindcss/tailwind.css';
 // import { CartProvider } from 'hooks/Cart/CartOrders';
@@ -20,6 +20,23 @@ import FirebaseRegister from 'views/pages/authentication/auth-forms/AuthRegister
 import CartManager from 'Helpers/CartManager';
 
 // ==============================|| APP ||============================== //
+// Define a function to get a cookie by its name
+function getCookie(name) {
+  const cookieName = name + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(';');
+
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return null;
+}
 
 const App = () => {
   const customization = useSelector((state) => state.customization);
@@ -31,18 +48,28 @@ const App = () => {
   axios.interceptors.response.use(
     (response) => response,
     (error) => {
-     if (error.response.status === 401) {
+      if (error.response && error.response.status === 401) {
         if (window.location.pathname !== '/register') {
           console.log('Redirecting to login');
           navigate('/login');
         }
       }
-
-
       return Promise.reject(error);
     }
   );
   
+  axios.interceptors.request.use(
+    (config) => {
+      const token = getCookie("Authtoken"); 
+      if (token) {
+        navigate('/dashboard');
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
 
   return (
     <StyledEngineProvider injectFirst>
